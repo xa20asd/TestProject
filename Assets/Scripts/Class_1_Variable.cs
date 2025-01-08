066d3a5d4a0b56d415273879bf27dba3fbe3c90f
xa20asd <xa20asd@gmail.com>
2025-01-08 20:53:35 +0800

    Update 資料類型

--- Assets/Scripts/Class_1_Variable.cs
+++ Assets/Scripts/Class_1_Variable.cs
@@ -10,7 +10,8 @@ using UnityEngine;
 
 
 //修飾詞  類別關鍵字(腳本) 類別名稱(腳本名稱)
-public class Class_1_Variable
+//在腳本後添加: MonoBehaviour 可以讓此腳本掛在Unity的物件上
+public class Class_1_Variable : MonoBehaviour 
 {
     //程式內的括號都是成對出現 () [] <> '' ""
     //class {}  此類別的程式內容
@@ -19,10 +20,15 @@ public class Class_1_Variable
     //定義一個記憶體內的空間用來存放資訊
 
     //變數語法:
-    //變數資料類型_變數名稱;
+    //修飾詞變數資料類型_變數名稱;
+    //修飾詞變數資料類型_變數名稱 指定 預設值; (指定 預設值 可省略)
     //資料類型: 告訴記憶體可以放入哪些資料
     //變數名稱: 習慣用小寫開頭命名，駝峰式命名，規則與腳本名稱相同
-    int coin;
+    //公開:允許其他類別存取，顯示在屬性面板上 public
+    //私人: 禁止其他類別存取，隱藏在屬性面板上private (預設)
+
+    public int coin = 500;
+    private int level = 3;
 }
 
 //類別外，不要將程式寫在這裡
---
+++ Assets/Scripts/Class_2_DataType.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+//  3/// 是摘要(xml)，用來簡要說明，可加在類別與變數上方
+/// <summary>
+/// 認識C# 資料類型
+/// </summary>
+
+public class Class_2_DataType : MonoBehaviour 
+{
+    //變數語法:
+    //修飾詞 資料類型 變數名稱 指定 預設值;
+    //四大常用資料類型
+    //整數:儲存沒有小數點的數值 int
+    //浮點數:儲存有小數點的數值 float
+    //字串: 儲存文字，必須用"" string
+    //布林值: 儲存正與反 bool 只有2種值 true & false
+    public int count = 7;
+    public float moveSpeed = 3.5f; //浮點數必須要加 f 後綴詞
+    public float turnSpeed = 30.0F; //大小 F 皆可
+    public string character = "Monster";
+    public bool isDead = false;
+    public bool gameOver = true;
+}
