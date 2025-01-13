380dc27a80994afbd0e9361e940da65e91d4de65
xa20asd <xa20asd@gmail.com>
2025-01-13 19:12:59 +0800

    完成作業練習

---
+++ Assets/Scripts/課程_1_變數.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class 課程_1_變數 :MonoBehaviour
+{
+    public int price = 999;
+    private float jumpHeight = 77.5f;
+    public string weapon = "三項之力";
+    public bool getSoul = true;
+}
---
+++ Assets/Scripts/課程_2_資料類型.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+public class 課程_2_資料類型 : MonoBehaviour
+{
+    public float number1 = 7.7f;
+    public double number2 = 0.12345;
+    public decimal number3 = 1.23456789m;
+}
