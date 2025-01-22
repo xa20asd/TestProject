d7e3d1e84bc9207e5078526ba1919b210c5d53d6
xa20asd <xa20asd@gmail.com>
2025-01-22 22:49:02 +0800

    upload 作業3

---
+++ Assets/Practice/課程_5_判斷式與列舉.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class 課程_5_判斷式與列舉: MonoBehaviour
+{
+    [SerializeField, Header("血量"), Range(0, 100)]
+    public int hp = 100;
+    private void Update()
+    {
+        switch (hp)
+        {
+            case >= 80:
+               Debug.Log("血量安全");
+                break;
+            case >= 60:
+                Debug.Log("健康狀態有狀況");
+                break;
+            case  >= 40:
+                Debug.Log("警告，快喝補的");
+                break;
+            case  >= 10:
+                Debug.Log("快死掉了");
+                break;
+            case  0:
+                Debug.Log("你眼前一黑，死掉了");
+                break;
+            default:
+                break;
+        }
+    }
+}
---
+++ Assets/Practice/課程_6_迴圈與陣列.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class 課程_6_迴圈與陣列: MonoBehaviour
+{
+    //宣告三維陣列
+    public string[,,] character =
+    {
+        {{"代歐奇希斯", "皮卡丘" },{"烈空座", "火雉雞"}, {"水躍魚", "新葉喵"}},
+        {{"綠寶", "木妖" },{"露希妲", "炎魔"}, {"菇菇寶貝", "黑龍王"}},
+        {{"提摩", "拉克絲" },{"維珈", "蓋倫"}, {"葛雷夫", "阿祈爾"}}
+    };
+
+    private void Awake()
+    {
+        Debug.Log($"<color=yellow>神奇寶貝:{character[0,0,1]}</color>");
+        Debug.Log($"<color=orange>楓之谷怪物:{character[1,2,0]}</color>");
+        Debug.Log($"<color=green>英雄聯盟角色:{character[2,1,1]}</color>");
+        Debug.Log($"<color=aqua>角色[1,0,2]:{character[1,0,2]}</color>");
+    }
+}
