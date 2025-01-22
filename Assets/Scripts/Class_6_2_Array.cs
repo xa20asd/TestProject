97263e3ba0183d319988bf4520a66d9853400e9c
xa20asd <xa20asd@gmail.com>
2025-01-22 21:43:40 +0800

    Update 認識三維陣列

--- Assets/Scripts/Class_6_2_Array.cs
+++ Assets/Scripts/Class_6_2_Array.cs
@@ -24,7 +24,12 @@ namespace honordes
 
         //二維陣列
         public string[,] inventory = { { "紅水", "藍水" }, { "炸彈", "金幣" } };
-
+        //三維陣列
+        public string[,,] shop =
+        {
+            {{"傷藥","好傷藥" },{"PP噴霧","白金噴霧" } },
+            {{"紅水","超水" },{"蘋果","檸檬" } }
+        };
         private void Awake()
         {
             #region 一維陣列
@@ -41,11 +46,31 @@ namespace honordes
             Debug.Log($"<color=aqua>Deck2的第3張卡牌:{deck2[2]}</color>");
             #endregion
 
+            #region 二維三維陣列
             //存取二維陣列
-            Debug.Log($"<color=green>編號[0,1]的道具:{inventory[0,1]}</color>");
+            Debug.Log($"<color=green>編號[0,1]的道具:{inventory[0, 1]}</color>");
 
             inventory[1, 1] = "寶箱";
             Debug.Log($"<color=green>編號[1,1]的道具:{inventory[1, 1]}</color>");
+
+
+            //存取三維陣列
+            Debug.Log($"<color=yellow>編號[0,1,1]的道具:{shop[0, 1, 1]}</color>");
+
+            shop[1, 0, 1] = "全滿藥水";
+            Debug.Log($"<color=yellow>編號[1,0,1]的道具:{shop[1, 0, 1]}</color>");
+            #endregion
+
+            //獲得陣列的長度或維度
+            //一維~多維陣列的長度: 陣列名稱.Length
+            Debug.Log($"<color=red>一維陣列長度:{deck2.Length}</color>");
+            Debug.Log($"<color=red>二維陣列長度:{inventory.Length}</color>");
+            Debug.Log($"<color=red>三維陣列長度:{shop.Length}</color>");
+
+            //陣列的維度: 陣列名稱.Rank
+            Debug.Log($"<color=red>一維陣列維度:{deck2.Rank}</color>");
+            Debug.Log($"<color=red>二維陣列維度:{inventory.Rank}</color>");
+            Debug.Log($"<color=red>三維陣列維度:{shop.Rank}</color>");
         }
     }
 }
