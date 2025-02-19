7d9b5a07307fc7f0bc0ee6bc9febae505e8d9960
xa20asd <xa20asd@gmail.com>
2025-02-19 21:43:32 +0800

    Update 認識多型

--- Assets/Scripts/Class_10_1_Inherit.cs
+++ Assets/Scripts/Class_10_1_Inherit.cs
@@ -1,5 +1,4 @@
 ﻿using UnityEngine;
-using honordes;
 using honordes.tools;
 
 namespace honordes
@@ -13,8 +12,22 @@ namespace honordes
         {
             var goblin = new Goblin(10, 5);
             Tools.LogMessage($"哥布林大哥的攻擊:{goblin.attack}", "green");
-            var slime = new Slime(3, 2);
-            Tools.LogMessage($"史萊姆小弟的攻擊:{slime.attack}", "green");
+            var slime1 = new Slime(3, 2);
+            Tools.LogMessage($"史萊姆小弟的攻擊:{slime1.attack}", "green");
+            goblin.Move();
+            slime1.Move();
+            slime1.Initilize();
+
+            //判斷史萊姆是否繼承哥布林
+            //類別A is 類別B : 布林值
+            var slimeIsGoblin = slime1 is Goblin;
+            Tools.LogMessage($"史萊姆是否為哥布林:{slimeIsGoblin}", "red");
+
+            var spider1 = new Spider(15, 3);
+            var spider1IsSlime = spider1 is Slime;
+            var spider1IsGoblin = spider1 is Goblin;
+            Tools.LogMessage($"蜘蛛人是否為史萊姆:{spider1IsSlime}", "red");
+            Tools.LogMessage($"蜘蛛人是否為哥布林:{spider1IsGoblin}", "red");
 
         }
     }
@@ -25,6 +38,7 @@ namespace honordes
         public int defense;
         private float moveSpeed;    //私人: 此類別可以存取
         protected float hp;         //保護: 子類別可以存取
+        protected int lv = 8;
 
         public Goblin(int _attack, int _defense)
         { 
@@ -32,14 +46,20 @@ namespace honordes
             defense = _defense;
         }
 
+        // virtual 虛擬: 允許子類別覆寫
+        public virtual void Move()
+        {
+            Tools.LogMessage("2隻腳移動", "orange");
+        }
+
     }
 
     // 繼承讓類別可以繼承另一個類別，擁有該類別的成員
     // 子類別 :　父類別
     // C# 僅能提供單一繼承(只能繼承一個類別)
-
     public class Slime : Goblin
-    { 
+    {
+        protected int lv = 3; // 綠色蚯蚓: 提示父類別也有相同的成員名稱
         public Slime(int _attack, int _defense) : base(_attack, _defense)
         {
             
@@ -50,9 +70,29 @@ namespace honordes
             attack = 4;         // 公開:可以存取
             //moveSpeed = 5.2f;   // 私人: 無法存取
             hp = 50;            // 保護: 可以存取
+            Tools.LogMessage($"等級:{lv}", "yellow");
+            Tools.LogMessage($"等級:{base.lv}(父類別)", "yellow");
         }
 
+        // override 覆寫: 覆寫父類別有 virtual 關鍵字的成員
+        // override 選擇要覆寫的成員 > 按下Enter自動完成
+        public override void Move()
+        {
+            //base.Move();  //父類別原有的內容
+            Tools.LogMessage("爬行", "aqua");
+        }
+
+        
+
     }
-    
-     
+
+    public class Spider : Slime
+    {
+        public Spider(int _attack, int _defense) : base(_attack, _defense)
+        {
+
+        }
+    }
+
+
 }
---
+++ Assets/Scripts/Class_10_2_Polymorphism.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using honordes.tools;
+
+namespace honordes
+{
+    /// <summary>
+    /// 多型 Polymorphism
+    /// </summary>
+    public class Class_10_2_Polymorphism: MonoBehaviour
+    {
+        private void Awake()
+        {
+            Trap trap1 = new Trap("落穴", 3);
+            Magic magic1 = new Magic("死者復甦", 5);
+
+            //多型: 多種型式，可以使用父類別或者自己的成員
+            trap1.Information();    //型式1 :呼叫父類別的無參數方法
+            trap1.Information(37);  //型式2 :呼叫自己有一個參數方法
+
+            //多型: 宣告時使用父類別
+            Card card1 = new Card("一般卡片", 0);
+            Card magic2 = new Magic("落雷", 5);
+            card1.Information();    //型式1:呼叫Card的方法
+            magic2.Information();   //型式2:呼叫Magic的方法
+
+        }
+    }
+    public class Card
+    {
+        public string name;
+        public int cost;
+        public Card(string _name, int _cost)
+        {
+            name = _name;
+            cost = _cost;
+        }
+        public virtual void Information()
+        {
+            Tools.LogMessage($"{name}這是一張卡牌", "aqua");
+        }
+    }
+    public class Trap : Card
+    {
+        //public Trap(string _name, int _cost)
+        //{
+        //}
+        public void Information(int index)
+        {
+            Tools.LogMessage($"這是一張陷阱卡牌，編號:{index}", "orange");
+        }
+    }
+    public class Magic : Card
+    {
+        //public Magic(string _name, int _cost)
+        //{ 
+        //}
+        public override void Information()
+        {
+            Tools.LogMessage($"{name}，這是一張魔法卡牌", "green");
+        }
+    }
+}
