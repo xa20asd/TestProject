14e9bd5a51427d0b00703706aaef4f8d4f7c2af8
xa20asd <xa20asd@gmail.com>
2025-03-10 19:51:37 +0800

    Update 實作介面

---
+++ Assets/Scripts/Class_15_Interface.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+
+namespace honordes
+{
+    /// <summary>
+    /// 介面 Interface
+    /// </summary>
+    public class Class_15_Interface :MonoBehaviour
+    {
+        public object invertoryFirst;
+        private void Awake()
+        {
+            //var prop = new Prop();
+            //var equip = new Equipment();
+            //var map = new Map();
+
+            //prop.Use();
+            //equip.Use();
+            //map.Use();
+
+            int random = Random.Range(0, 3);
+            Debug.Log($"隨機: {random}");
+            
+            if(random == 0) invertoryFirst = new Prop();
+            else if (random == 1) invertoryFirst = new Equipment();
+            else if (random == 2) invertoryFirst = new Map();
+        }
+
+        private void Update()
+        {
+            // 按下數字鍵 1 可以使用第一個物品
+            if (Input.GetKeyDown(KeyCode.Alpha1))
+            {
+                // 如果第一格是道具，就使用道具
+                if (invertoryFirst is Prop) ((Prop)invertoryFirst).Use();
+
+                // 如果第一格是裝備，就使用裝備
+                if (invertoryFirst is Equipment) ((Equipment)invertoryFirst).Use();
+
+                // 如果第一格是地圖，就使用地圖
+                if (invertoryFirst is Map) ((Map)invertoryFirst).Use();
+
+                // 如果當項目種類越多之後，判斷式會越來越長...
+            }
+        }
+
+
+
+    }
+    public class Prop
+    {
+        public void Use()
+        {
+            Debug.Log("使用道具");
+        }
+    }
+    public class Equipment
+    {
+        public void Use()
+        {
+            Debug.Log("使用裝備，裝到對應位置");
+        }
+    }
+    public class Map
+    {
+        public void Use()
+        {
+            Debug.Log("使用地圖，開啟地圖功能");
+        }
+    }
+}
---
+++ Assets/Scripts/Class_15_InterfaceObject.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.SocialPlatforms;
+
+namespace honordes
+{
+    /// <summary>
+    /// 介面物件 : 實作介面的物件
+    /// </summary>
+    public class Class_15_InterfaceObject
+    {
+
+    }
+
+    // 介面使用步驟
+    // 1. 定義介面與介面成員
+    // 2. 類別實作介面
+    // 3. 實作介面成員
+
+    // 步驟 1
+    // 帶有「使用」功能的介面，遊戲內可被使用的物品
+    public interface IUse
+    {
+        // 不用宣告方法主體，不用寫大括號
+        public void Use();
+    }
+
+    // 步驟2.
+    // C#單一繼承，多重實作介面
+    public class Weapon : IUse
+    {
+        public void Use()
+        {
+            Debug.Log("使用武器，施展武器技能");
+        }
+    }
+    public class Potion : IUse
+    {
+        public void Use()
+        {
+            Debug.Log("使用藥水，恢復體力");
+        }
+    }
+    public class Chest : IUse
+    {
+        public void Use()
+        {
+            Debug.Log("使用寶箱，獲得隨機道具");
+        }
+    }
+}
